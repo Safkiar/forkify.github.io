27eb6090455bc9e178978b7cbc400556c040a77d
Safkiar <m.kikowski97@gmail.com>
2024-03-14 16:20:33 +0100

    Patch 3. Added listening for load and hashchange events

--- src/js/controller.js
+++ src/js/controller.js
@@ -26,12 +26,17 @@ const renderSpinner = function (parentEl) {
   parentEl.insertAdjacentHTML(`afterbegin`, markup);
 };
 
-const showReccipe = async function () {
+const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+
     // loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc13`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -150,4 +155,8 @@ const showReccipe = async function () {
     alert(err);
   }
 };
-showReccipe();
+showRecipe();
+
+[`hashchange`, `load`].forEach(ev => window.addEventListener(ev, showRecipe));
+// window.addEventListener(`hashchange`, showRecipe);
+// window.addEventListener(`load`, showRecipe);
