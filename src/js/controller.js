0eb672a2e37ee7893a244fe90cd2df6cf6891bd8
Safkiar <m.kikowski97@gmail.com>
2024-03-19 22:11:37 +0100

    pagination implemented

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,20 +2,22 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
 
-if (module.hot) {
-  module.hot.accept();
-} // parcel thing
+// if (module.hot) {
+//   module.hot.accept();
+// } // parcel thing
 
 // https://forkify-api.herokuapp.com/v2
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
+    console.log('first step: taking ID from hash', id); // c1
 
     if (!id) return;
 
@@ -39,15 +41,23 @@ const controlSearchResults = async function () {
 
     await model.loadSearchResults(query);
 
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSerchResultsPage());
+
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  resultsView.render(model.getSerchResultsPage(goToPage));
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 
 init();
--- src/js/views/view.js
+++ src/js/views/view.js
@@ -10,6 +10,8 @@ export default class View {
     const markup = this._generateMarkup();
     this._clear();
     this._parentElement.insertAdjacentHTML(`afterbegin`, markup);
+
+    console.log(data, 'halo');
   }
 
   _clear() {
